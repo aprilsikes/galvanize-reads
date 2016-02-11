469921498d4c25cb857edfb7de47f0383fa7d848
april sikes <aprilsikes@gmail.com>
2016-02-10 17:27:01 -0700

    add author

--- routes/authors.js
+++ routes/authors.js
@@ -18,4 +18,25 @@ router.get('/', function (req, res, next) {
   })
 })
 
+router.get('/new', function (req, res, next) {
+  res.render('authors/new');
+})
+
+router.post('/', function (req, res, next) {
+  var author = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    biography: req.body.biography,
+    portrait_url: req.body.portrait_url
+  }
+  var errors = validator(req.body);
+  if (errors.length) {
+    res.render('authors/new', {info: req.body, errors: errors});
+  } else {
+    Authors().insert(req.body).then(function (results) {
+      res.redirect('/authors');
+    })
+  }
+})
+
 module.exports = router;
